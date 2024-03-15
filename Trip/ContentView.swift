3995a512fc501ffec9bc8cc43d0efd9d3f54d49c
ShunyaFair <mirandasaikou@gmail.com>
2024-03-15 09:50:18 +0900

    Initial Commit

---
+++ Trip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trip
+//
+//  Created by 香川隼也 on 2024/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trip/TripApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripApp.swift
+//  Trip
+//
+//  Created by 香川隼也 on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct TripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
