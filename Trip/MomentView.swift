28e5a5251118d5ae3877487365fa5eced5159af2
ShunyaFair <mirandasaikou@gmail.com>
2024-03-15 11:33:00 +0900

    1

---
+++ Trip/MomentView.swift
@@ -0,0 +1,8 @@
+//
+//  MomentView.swift
+//  Trip
+//
+//  Created by 香川隼也 on 2024/03/15.
+//
+
+import Foundation
---
+++ Trip/RuleDescriptionView.swift
@@ -0,0 +1,8 @@
+//
+//  RuleDescriptionView.swift
+//  Trip
+//
+//  Created by 香川隼也 on 2024/03/15.
+//
+
+import Foundation
